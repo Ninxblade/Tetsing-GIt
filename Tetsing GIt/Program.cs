c9e76ff707c49571a68c700b5c6482a742e30287
JasonLi <jasonli321@hotmail.ca>
2019-07-16 23:19:47 -0400

    Add project files.

---
+++ Tetsing GIt/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tetsing_GIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing push to GIT Hub");
+        }
+    }
+}
